3eef9f704f4306ee0fa696adcc0228ebc66c2844
test <okawatakanori9916@gmail.com>
2021-05-31 00:08:53 +0900

    init

--- src/components/vue-awesome-swiper-v2.vue
+++ src/components/vue-awesome-swiper-v2.vue
@@ -2,7 +2,7 @@
   → vue-awesome-swiperはvue2から使える。
   vue3では試していない。（エラーが出たので、vue2に切り替えた）
 （公式）https://github.com/surmon-china/vue-awesome-swiper
-  swiper.jsもインストールする必要がある。
+  swiper.jsもインストールする必要がある。swiperはversion5が推奨されている
   読み込むcssはswiperのバージョンによってディレクトリが異なるので注意
 -->
 
@@ -10,19 +10,15 @@
   <div class="contents">
     <h1>VueAwesomeSwiperを使う</h1>
     <swiper :options="swiperOption">
-      <swiper-slide
-        >Slide 1
+      <swiper-slide>
         <img src="../assets/image_1.jpg" />
       </swiper-slide>
-      <swiper-slide
-        >Slide 2
+      <swiper-slide>
         <img src="../assets/image_2.jpg" />
       </swiper-slide>
-      <swiper-slide
-        >Slide 3
+      <swiper-slide>
         <img src="../assets/image_3.jpg" />
       </swiper-slide>
-      ...
     </swiper>
   </div>
   <!-- /.contents -->
@@ -31,25 +27,33 @@
 <script>
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
 
-// スタイルもモジュールから読み込む
+// スタイル読み込み
 import "swiper/css/swiper.css";
 
 export default {
   data() {
     return {
+      // optionはtemplateにディレククティブ的に書くこともできる。
+      // 例 <swiper :slides-per-viwe="3" :space-between="10"></swiper>
+
+      // 下のようにdataにまとめて渡すこともできる。
       swiperOption: {
-        effect: "flip", // 切り替わりの見た目変化
-        grabCursor: true, // カーソルがグローブになる。
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        slidesPerView: 1, // 表示枚数
-        centeredSlides: true,
+        freeMode: true,
         loop: true,
+        loopedSlides: 6,
+        slidesPerView: 3,
+        speed: 3000,
         autoplay: {
-          delay: 2000,
-          disableOnInteraction: false,
+          delay: 0,
+        },
+
+        // スライドが止まらず、ひたすら動くための処理
+        // （参考）https://tedate.jp/wp-tips/swiper-horizontal-scroll-continuously
+        on: {
+          slideChangeTransitionStart: function () {
+            const wrapper = document.querySelector(".swiper-wrapper");
+            wrapper.style.transitionTimingFunction = "linear";
+          },
         },
       },
     };
@@ -62,14 +66,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+* {
+  margin: 0;
+}
 h1 {
   text-align: center;
-}
-
-// swiperの全体スタイル
-.swiper-container {
-  width: 300px;
-  height: 300px;
 }
 
 // // スライドの各カードのスタイル
